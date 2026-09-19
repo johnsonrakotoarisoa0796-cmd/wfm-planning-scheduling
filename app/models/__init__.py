0066e6071a3940962573bd10bff0045e0617c96d
johnsonrakotoarisoa0796-cmd <johnson.rakotoarisoa0796@gmail.com>
2026-09-19 16:29:21 +0000

    feat(wfm): register market model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,6 +7,7 @@ ajoutee ici.
 """
 
 from app.models.campaign import Campaign
+from app.models.market import Market
 from app.models.client_stf import ClientSTFInterval, ClientSTFPlan
 from app.models.capacity import CapacityPlan
 from app.models.config_parameter import ConfigParameter
@@ -23,6 +24,7 @@ from app.models.user import User
 
 __all__ = [
     "Campaign",
+    "Market",
     "ClientSTFInterval",
     "ClientSTFPlan",
     "CapacityPlan",
